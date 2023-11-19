3215f22e16577966a6faeedfe5f699e22f5442af
Alex Schwartz <alexschwartz01@gmail.com>
2023-11-19 18:31:25 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,14 +1,6 @@
 sudo apt update
 sudo apt install podman -y
-
-# CHANGEME to
-#
-# curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh
-#
-# when the latest release of Distrobox supports Crostini
-
-curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh -s -- --next
-
+curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh
 echo "$USER:10000:65536" | sudo tee -a /etc/subgid
 echo "$USER:10000:65536" | sudo tee -a /etc/subuid
 podman system migrate
