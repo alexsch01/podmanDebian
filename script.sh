1027f887dfbef8039be58dd0a3770ddbe61763bf
Alex Schwartz <alexschwartz01@gmail.com>
2025-02-28 14:19:00 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,3 @@
 sudo apt update
 sudo apt install podman -y
-curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh
 sudo usermod --add-subuids 100000-165535 --add-subgids 100000-165535 $USER
